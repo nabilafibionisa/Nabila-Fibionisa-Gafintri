7d99679a14892cba8abeb525160a2b062f5c9163
Nabila Fibionisa Gafintri <82861622+nabilafibionisa@users.noreply.github.com>
2021-04-21 18:30:55 +0700

    Add files via upload

---
+++ halo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# /------------------------------------/                         
+# /    program halo.py                 /                                        
+# /  by nabilafibionisa01@mail.ugm.id  /
+# /------------------------------------/
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
