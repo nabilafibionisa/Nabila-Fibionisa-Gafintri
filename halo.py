f7505c3881e299385b9a000104249af0e134f07f
Nabila Fibionisa Gafintri <82861622+nabilafibionisa@users.noreply.github.com>
2021-04-21 18:47:21 +0700

    Add files via upload

--- halo.py
+++ halo.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[4]:
 
 
-# /------------------------------------/                         
-# /    program halo.py                 /                                        
-# /  by nabilafibionisa01@mail.ugm.id  /
-# /------------------------------------/
-
-
-# In[ ]:
-
-
-
+print ("#/=====================================/")
+print ("#/                                     /")
+print ("#/         program hello.py            /")
+print ("#/ by nabilafibionisa01@mail.ugm.ac.id /")
+print ("#/                                     /")
+print ("#/=====================================/")
 
 
 # In[ ]:
